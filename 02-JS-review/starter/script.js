e26c9257817ebd3b59744c4d28a436c7781c1d6c
David <daveman@gmail.com>
2023-08-26 21:28:08 -0700

    setup index.js from scratch

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -142,3 +142,12 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+const book = getBook(2);
+
+// const title = book.title
+// const author = book.author
+
+const { title, author, pages, publicationDate, genres, hasMovieAdaptation } = book;
+
+book
