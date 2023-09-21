84b2dcae78c12c34a938a9742b4bbb87b53027f4
David <daveman@gmail.com>
2023-09-21 04:18:13 -0700

    improve usability of component

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+//allows access to internal state
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        color="blue"
+        maxRating={10}
+        onSetRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={2}
+    />
+    <StarRating
+      size={24}
+      color="red"
+      maxRating={5}
+      className="test"
+      defaultRating={3}
+    />
+    <Test />
     {/* <App /> */}
   </React.StrictMode>
 );
