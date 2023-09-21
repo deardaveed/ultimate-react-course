d0d049d16ae6396cc815c4d66aa1354752382d9b
David <daveman@gmail.com>
2023-09-21 02:50:49 -0700

    work on star rating system

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -109,27 +109,6 @@ function Box({ children }) {
   );
 }
 
-// function WatchedBox() {
-//   const [watched, setWatched] = useState(tempWatchedData);
-//   const [isOpen2, setIsOpen2] = useState(true);
-
-//   return (
-//     <div className="box">
-//       <button
-//         className="btn-toggle"
-//         onClick={() => setIsOpen2(open => !open)}>
-//         {isOpen2 ? "–" : "+"}
-//       </button>
-//       {isOpen2 && (
-//         <>
-//           <WatchedSummary watched={watched} />
-//           <WatchedMoviesList watched={watched} />
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-
 function Logo() {
   return (
     <div className="logo">
